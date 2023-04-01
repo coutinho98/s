fd71f01703e2389cdaaa366ece9430467d0f13db
Mateus Couto <mateuscouto98@outlook.com>
2023-04-01 15:08:11 -0300

    slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,83 @@
 'use strict';
 
-const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
-const header = document.querySelector("[data-header]");
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
 
-navToggleBtn.addEventListener("click", function () {
-  header.classList.toggle("active");
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on');
+}
+
+function nextSlider() {
+  hideSlider() 
+  if(currentSlide === slider.length -1 ) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider() 
+  if(currentSlide === 0) {
+    currentSlide = slider.length - 1 
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnPrev.addEventListener('click', prevSlider);
+btnNext.addEventListener('click', nextSlider);
+
+const navToggleBtn = document.querySelector('[data-nav-toggle-btn]');
+const header = document.querySelector('[data-header]');
+
+navToggleBtn.addEventListener('click', function () {
+  header.classList.toggle('active');
 });
+
+
+/* const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider) */
